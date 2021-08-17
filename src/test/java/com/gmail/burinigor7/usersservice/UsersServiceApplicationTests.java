10126a0a559ae4831e1caaa001e6c324db4803a9
Igor Burin <iburin@c710.dlink>
2021-08-17 22:49:04 +0400

    local profile db connection & pom changes

--- src/test/java/com/gmail/burinigor7/usersservice/UsersServiceApplicationTests.java
+++ src/test/java/com/gmail/burinigor7/usersservice/UsersServiceApplicationTests.java
@@ -2,8 +2,10 @@ package com.gmail.burinigor7.usersservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("local")
 class UsersServiceApplicationTests {
 
     @Test
