5a6a624832e793680905e442bd0e9ff659c5f88e
Igor Burin <iburin@c710.dlink>
2021-08-21 15:59:09 +0400

    making user resource api hypermedia driven

--- src/main/java/com/gmail/burinigor7/usersservice/controller/UserController.java
+++ src/main/java/com/gmail/burinigor7/usersservice/controller/UserController.java
@@ -4,8 +4,13 @@ import com.gmail.burinigor7.usersservice.domain.Role;
 import com.gmail.burinigor7.usersservice.domain.User;
 import com.gmail.burinigor7.usersservice.exception.UserNotFoundException;
 import com.gmail.burinigor7.usersservice.service.UserService;
+import com.gmail.burinigor7.usersservice.util.UserModelAssembler;
 import lombok.RequiredArgsConstructor;
+import org.springframework.hateoas.CollectionModel;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.IanaLinkRelations;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,64 +23,103 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
 public class UserController {
     private final UserService userService;
+    private final UserModelAssembler assembler;
 
     @GetMapping("/{id}")
-    public User user(@PathVariable Long id) {
-        return userService.user(id);
+    public EntityModel<User> user(@PathVariable Long id) {
+        User user = userService.user(id);
+        return assembler.toModel(user);
     }
 
     @GetMapping
-    public Iterable<User> all() {
-        return userService.all();
+    public CollectionModel<EntityModel<User>> all() {
+        List<EntityModel<User>> userModels = StreamSupport
+                .stream(userService.all().spliterator(), false)
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(userModels,
+                linkTo(methodOn(UserController.class).all()).withSelfRel());
     }
 
     @GetMapping(params = "email")
-    public User userByEmail(@RequestParam String email) {
-        return userService.userByEmail(email);
+    public EntityModel<User> userByEmail(@RequestParam String email) {
+        User user = userService.userByEmail(email);
+        return assembler.toModel(user);
     }
 
     @GetMapping(params = "login")
-    public User userByLogin(@RequestParam String login) {
-        return userService.userByLogin(login);
+    public EntityModel<User> userByLogin(@RequestParam String login) {
+        User user = userService.userByLogin(login);
+        return assembler.toModel(user);
     }
 
     @GetMapping("/filter-by-name")
-    public List<User> usersByName(
+    public CollectionModel<EntityModel<User>> usersByName(
             @RequestParam(value = "first-name", required = false) String firstName,
             @RequestParam(value = "last-name", required = false) String lastName,
             @RequestParam(value = "patronymic", required = false) String patronymic) {
-        return userService.usersByName(firstName, lastName, patronymic);
+        String aggregateRootRel = "users";
+        List<EntityModel<User>> userModels = userService
+                .usersByName(firstName, lastName, patronymic)
+                .stream()
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(userModels,
+                linkTo(methodOn(UserController.class)
+                        .usersByName(firstName, lastName, patronymic))
+                        .withSelfRel(),
+                linkTo(methodOn(UserController.class).all()).withRel(aggregateRootRel));
     }
 
     @GetMapping(params = "role")
-    public Iterable<User> usersByRole(@RequestParam Role role) {
-        return userService.usersByRole(role);
+    public CollectionModel<EntityModel<User>> usersByRole(@RequestParam Role role) {
+        String aggregateRootRel = "users";
+        List<EntityModel<User>> userModels = StreamSupport
+                .stream(userService.usersByRole(role).spliterator(), false)
+                .map(assembler::toModel)
+                .collect(Collectors.toList());
+        return CollectionModel.of(userModels,
+                linkTo(methodOn(UserController.class).usersByRole(role))
+                        .withSelfRel(),
+                linkTo(methodOn(UserController.class).all())
+                        .withRel(aggregateRootRel));
     }
 
     @GetMapping(params = "phone-number")
-    public User usersByPhoneNumber(@RequestParam("phone-number") String phoneNumber) {
-        return userService.userByPhoneNumber(phoneNumber);
+    public EntityModel<User> usersByPhoneNumber(@RequestParam("phone-number") String phoneNumber) {
+        User user = userService.userByPhoneNumber(phoneNumber);
+        return assembler.toModel(user);
     }
 
     @PostMapping
-    public User newUser(@RequestBody User user) {
-        return userService.newUser(user);
+    public ResponseEntity<EntityModel<User>> newUser(@RequestBody User user) {
+        EntityModel<User> userModel = assembler.toModel(userService.newUser(user));
+        return ResponseEntity.created(userModel.getRequiredLink(IanaLinkRelations.SELF)
+                .toUri()).body(userModel);
     }
 
     @PutMapping("/{id}")
-    public User replaceUser(@RequestBody User newUser, @PathVariable Long id) {
-        return userService.replaceUser(newUser, id);
+    public ResponseEntity<EntityModel<User>> replaceUser(@RequestBody User newUser, @PathVariable Long id) {
+        EntityModel<User> userModel = assembler.toModel(userService.replaceUser(newUser, id));
+        return ResponseEntity.created(userModel.getRequiredLink(IanaLinkRelations.SELF)
+                .toUri()).body(userModel);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteUser(@PathVariable Long id) {
+    public ResponseEntity<?> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
+        return ResponseEntity.noContent().build();
     }
 
     @ExceptionHandler(UserNotFoundException.class)
