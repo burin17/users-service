63c12799c284a59ea43077d8dddf8b6b9a7da644
Igor Burin <iburin@c710.dlink>
2021-09-02 20:25:25 +0400

    spring cloud gateway service; service registry

--- api-gateway-service/src/main/java/com/gmail/burinigor7/apigatewayservice/ApiGatewayServiceApplication.java
+++ api-gateway-service/src/main/java/com/gmail/burinigor7/apigatewayservice/ApiGatewayServiceApplication.java
@@ -1,17 +1,15 @@
-package com.gmail.burinigor7.userscrudservice;
+package com.gmail.burinigor7.apigatewayservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
-@EnableRetry
 @EnableEurekaClient
-public class UsersCrudServiceApplication {
+public class ApiGatewayServiceApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(UsersCrudServiceApplication.class, args);
+        SpringApplication.run(ApiGatewayServiceApplication.class, args);
     }
 
 }
