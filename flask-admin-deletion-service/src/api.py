f841542e9500748bc234f80f0ac003723b0fb358
Igor Burin <burinigor7@gmail.com>
2021-09-07 21:59:51 +0400

    eureka client for python service

--- flask-admin-deletion-service/src/api.py
+++ flask-admin-deletion-service/src/api.py
@@ -1,14 +1,18 @@
 import flask
 import psycopg2
-
+import py_eureka_client.eureka_client as eureka_client
 import dbConfig
 
 from flask import request, jsonify
 
 app = flask.Flask(__name__)
+eureka_client.init(eureka_server="http://localhost:8761/eureka",
+                   app_name="admin-deletion-service",
+                   instance_port=9021)
 
 @app.route('/api/users/is-allowed/<id>', methods=['GET'])
 def isAllowed(id):
+    print('python instance')
     connection = psycopg2.connect(user=dbConfig.USER,
                                   password=dbConfig.PASSWORD,
                                   host=dbConfig.HOST,
@@ -18,4 +22,4 @@ def isAllowed(id):
     cursor.execute('select * from usr where id = %s', id)
     return jsonify(cursor.rowcount != 0)
 
-app.run(port = 9001)
+app.run(port = 9021)
